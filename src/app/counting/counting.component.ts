ad91e727144f6dd80c8146a8c18576579646305e
Krit <kritdevelop@gmail.com>
2021-02-22 22:27:42 +0700

    lab03 - link CountingComponent

---
+++ src/app/counting/counting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counting',
+  templateUrl: './counting.component.html',
+  styleUrls: ['./counting.component.css']
+})
+export class CountingComponent {
+  constructor() {}
+}
