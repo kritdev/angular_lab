b289337feeca516e2aece063d7ff9965bab99043
Krit <kritdevelop@gmail.com>
2021-02-23 10:54:21 +0700

    lab04 - create & link component

--- src/app/lab4/lab4.component.ts
+++ src/app/lab4/lab4.component.ts
@@ -1,3 +1,8 @@
 import { Component } from '@angular/core';
 
+@Component({
+  selector: 'lab4-component',
+  templateUrl: './lab4.component.html',
+  styleUrls: ['./lab4.component.css']
+})
 export class Lab4Component {}
