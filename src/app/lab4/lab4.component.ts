a0b3dac1f1c99727fd5ab62b932d97d65a69894b
Krit <kritdevelop@gmail.com>
2021-02-23 11:26:04 +0700

    lab04 - implement <p>

--- src/app/lab4/lab4.component.ts
+++ src/app/lab4/lab4.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './lab4.component.html',
   styleUrls: ['./lab4.component.css']
 })
-export class Lab4Component {}
+export class Lab4Component {
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  constructor() {}
+
+  isEven(item) {
+    return item % 2 === 0;
+  }
+}
