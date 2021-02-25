7f25664c48be879e06fbfe0a72d09d589a6f9a92
Krit <kritdevelop@gmail.com>
2021-02-25 20:48:48 +0700

    navbar - implement top-menu-left and top-menu-right

--- src/app/homework1/navbar/navbar.component.ts
+++ src/app/homework1/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css','./navbar.social-icon.css']
+  styleUrls: ['./navbar.component.css','./navbar.icon.css']
 })
 export class NavbarComponent {
   constructor() {}
