1377c28e19abcf28dc9dab3d75d386507964c4dc
Krit <kritdevelop@gmail.com>
2021-02-25 16:22:31 +0700

    homework1 - create home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CountingComponent } from './counting/counting.component';
-import { Lab4Component } from './lab4/lab4.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent, CountingComponent, Lab4Component
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
