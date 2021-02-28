78ad0855041aa8be5a46c12a9c1ffa0824bcc06d
Krit <kritdevelop@gmail.com>
2021-02-28 21:42:12 +0700

    navbar - implement search-bar component

--- src/app/homework1/search-bar/search-bar.component.ts
+++ src/app/homework1/search-bar/search-bar.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css','../navbar/navbar.icon.css']
 })
 export class SearchBarComponent {
   constructor() {}
