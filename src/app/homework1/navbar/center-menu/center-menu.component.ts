0d1d5916dd8dc16768c7f172453f3da9d3cfb44e
Krit <kritdevelop@gmail.com>
2021-02-28 22:43:20 +0700

    navbar - implement center-menu

---
+++ src/app/homework1/navbar/center-menu/center-menu.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'center-menu',
+  templateUrl: './center-menu.component.html',
+  styleUrls: ['../navbar.menu.css', './center-menu.component.css']
+})
+export class CenterMenuComponent {
+  constructor() {}
+}
