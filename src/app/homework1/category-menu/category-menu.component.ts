9596892ff6074326b8d5b7d4e1ddf6b94a5be4b4
Krit <kritdevelop@gmail.com>
2021-03-01 15:49:20 +0700

    homework1 - add ngFor

--- src/app/homework1/category-menu/category-menu.component.ts
+++ src/app/homework1/category-menu/category-menu.component.ts
@@ -6,5 +6,14 @@ import {Component} from '@angular/core';
   styleUrls: ['./category-menu.component.css']
 })
 export class CategoryMenuComponent {
+  menuItemList = [
+    {title:'โค้ดส่งฟรี', image:'../../../../assets/cat-menu1.png'},
+    {title:'แบรนด์แท้ถูกชัวร์', image:'../../../../assets/cat-menu2.png'},
+    {title:'Electronics Zone', image:'../../../../assets/cat-menu3.png'},
+    {title:'Fashion Zone Free Shipping', image:'../../../../assets/cat-menu4.png'},
+    {title:'Shopee Mart Free 100 Coins*', image:'../../../../assets/cat-menu5.png'},
+    {title:'โค้ดส่วนลด', image:'../../../../assets/cat-menu6.png'},
+    {title:'Partner Privileges', image:'../../../../assets/cat-menu7.png'}
+  ];
   constructor() {}
 }
