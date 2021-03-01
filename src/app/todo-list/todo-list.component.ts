cf0675ce81d55d51d8d0c0f1cb95b18cb7b413af
Krit <kritdevelop@gmail.com>
2021-03-01 17:51:42 +0700

    homework2 - create todo-list component

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: 'todo-list.component.html',
+  styleUrls: ['todo-list.component.css']
+})
+export class ToDoListComponent {
+
+  todoList = [];
+  task = '';
+
+  constructor() {}
+
+  addTask() {
+    this.todoList.push(this.task);
+    this.task = '';
+  }
+}
