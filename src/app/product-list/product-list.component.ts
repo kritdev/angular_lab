853df82acd642f611c21e17c0e3170b04405a32c
Krit <kritdevelop@gmail.com>
2021-03-08 13:52:18 +0700

    using @ViewChildren

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { Product } from '../models/product';
+import { ProductItemComponent } from '../product-item/product-item.component';
 
 @Component({
   selector: 'app-product-list',
@@ -7,6 +8,9 @@ import { Product } from '../models/product';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  @ViewChildren(ProductItemComponent)
+  productItems: QueryList<ProductItemComponent>;
+
   products: Product[];
 
   constructor() { 
@@ -16,7 +20,12 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  selectedProduct(product: Product) {
-    alert(`Product ${product.name} selected`);
+  selectedProduct(productComponent: ProductItemComponent) {
+    alert(`Product ${productComponent.product.name} selected`);
+    this.productItems.forEach(p => {
+      p.isSelected = false;
+      });
+      
+    productComponent.isSelected = true;
   }
 }
