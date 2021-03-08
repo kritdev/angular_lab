03cd1fec8cb8b3d96fc9e46bdbb47cf0d92fe463
Krit <kritdevelop@gmail.com>
2021-03-08 11:05:48 +0700

    Part02 - Receipt#2 - Lab 3

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from '../models/product';
 
 @Component({
@@ -10,9 +10,15 @@ export class ProductItemComponent implements OnInit {
   @Input()
   product: Product;
   
+  @Output()
+  OnSelected: EventEmitter<Product> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  select() {
+    this.OnSelected.emit(this.product);
+  }
 }
