0e10e022da1777f6358a996eec09b880b5b11a43
Krit <kritdevelop@gmail.com>
2021-03-08 20:09:04 +0700

    hind task-detail component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -18,30 +18,34 @@ export class ToDoListComponent {
 
   constructor() {
     this.task = new Task();
-    this.todoList.push({id:1, name:'task 1', description:'abcd12345678 000000000000000 00000000000000 0000000000000000 000000000000000'});
-    this.todoList.push({id:2, name:'task 2', description:'aaaaaaaaaaa8'});
-    this.todoList.push({id:3, name:'task 3', description:'ab5555555555'});
-    this.selectedTask = this.todoList[0];
   }
 
   addTask() {
     this.task.id = this.taskID++;
     this.todoList.push(this.task);
     this.task = new Task();
+
+    //clear selected task
+    this.selectTaskComponent(null);
   }
 
   deleteTask(task) {
     let index = this.todoList.findIndex( item => item.id === task.id);
     if(index >= 0 ) 
       this.todoList.splice(index, 1);
+
+    //clear selected task
+    this.selectTaskComponent(null);
   }
 
   selectTaskComponent(taskComponent) {
     this.taskList.forEach( item => item.isSelected = false);
-    taskComponent.isSelected = true;
 
-    //show task
-    this.selectedTask = taskComponent.task;
+    if(taskComponent) {
+      taskComponent.isSelected = true;
+      this.selectedTask = taskComponent.task;
+    } else {
+      this.selectedTask = null;
+    }
   }
-
 }
