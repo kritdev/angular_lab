2fef1352fa0f5d7c2010024767e7c9f769dc7838
Krit <kritdevelop@gmail.com>
2021-03-09 18:40:00 +0700

    generate panel components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TabPanelComponent } from './tab-panel/tab-panel.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { MainPanelComponent } from './main-panel/main-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabPanelComponent,
+    SidePanelComponent,
+    MainPanelComponent
   ],
   imports: [
     BrowserModule
