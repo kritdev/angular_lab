1f30f627603c5e631d0913d0b5d75e2ed69e8b5c
Krit <kritdevelop@gmail.com>
2021-03-11 22:21:09 +0700

    configuration-menu component - add submenu's indent

--- src/app/components/configuration-menu/configuration-menu.component.ts
+++ src/app/components/configuration-menu/configuration-menu.component.ts
@@ -11,6 +11,9 @@ export class ConfigurationMenuComponent implements OnInit {
   @Input()
   configurationMenu: ConfigurationMenu;
 
+  @Input()
+  menuLevel: number;
+
   constructor() { }
 
   ngOnInit(): void {
