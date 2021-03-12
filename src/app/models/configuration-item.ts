25a017b97cdca17ead238a83d865bd0bac93919b
Krit <kritdevelop@gmail.com>
2021-03-12 16:01:15 +0700

    implement configuration-item component

--- src/app/models/configuration-item.ts
+++ src/app/models/configuration-item.ts
@@ -2,7 +2,8 @@ export class ConfigurationItem {
   constructor(
     public id: String,
     public name: String,
-    public description: String
+    public description: String,
+    public isHeader: boolean = false
   ) {
   }
 }
