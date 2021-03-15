8b49f3e9cc98847d051fb31c8c65be18d8767b89
Krit <kritdevelop@gmail.com>
2021-03-15 08:29:56 +0700

    4.1 create Navbar and MainContent component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, MainContentComponent],
   imports: [
     CommonModule
   ]
