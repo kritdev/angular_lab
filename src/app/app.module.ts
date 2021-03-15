101562d9b28381499e90552e8aed1fc9a72ebbbb
Krit <kritdevelop@gmail.com>
2021-03-15 13:57:34 +0700

    modify AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 import { CatalogModule } from './catalog/catalog.module';
 import { HomeModule } from './home/home.module';
 
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomeComponent
+  // },
+  // {
+  //   path: 'home',
+  //   component: HomeComponent
+  // },
+  // {
+  //   path: 'login',
+  //   component: LoginComponent
+  // },
+  // {
+  //   path: 'products',
+  //   component: ProductListComponent
+  // },
+  // {
+  //   path: '**',
+  //   component: PageNotFoundComponent
+  // }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // HomeComponent,
+    // LoginComponent ,
+    // PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     CatalogModule,
     HomeModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
