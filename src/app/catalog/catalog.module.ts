e1134316952a81e63cfd18e638cd0a9f6b3cb126
Krit <kritdevelop@gmail.com>
2021-03-15 14:02:14 +0700

    create PanelComponent

--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductItemComponent } from './product-item/product-item.component'; 
+import { ProductItemComponent } from './product-item/product-item.component';
+import { PanelComponent } from './panel/panel.component'; 
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    PanelComponent
   ],
   imports: [
     CommonModule
