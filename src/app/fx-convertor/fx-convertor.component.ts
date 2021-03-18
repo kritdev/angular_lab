9b4c9698ab352db2f2200d81766d3b5201f4b4f4
Krit <kritdevelop@gmail.com>
2021-03-18 16:21:28 +0700

    Error handling

--- src/app/fx-convertor/fx-convertor.component.ts
+++ src/app/fx-convertor/fx-convertor.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-fx-convertor',
@@ -30,10 +32,19 @@ export class FxConvertorComponent implements OnInit {
 
     this.httpClient
     .get(`https://api.exchangeratesapi.io/latest?symbols=${fromCurrency},${toCurrency}`)
-    .subscribe(result => {
-      this.fxData = result as any[];
-      fxRate = this.fxData['rates'][toCurrency] / this.fxData['rates'][fromCurrency];
-      this.form.patchValue({toAmount: this.form.value.fromAmount * fxRate});
-    });
+    .pipe(
+      catchError(error => {
+        return throwError(
+          'Error in getting exchange rate.');
+      })
+    )
+    .subscribe(
+      result => {
+        this.fxData = result as any[];
+        fxRate = this.fxData['rates'][toCurrency] / this.fxData['rates'][fromCurrency];
+        this.form.patchValue({toAmount: this.form.value.fromAmount * fxRate});
+      }, 
+      err => {alert(err)}
+    );
   }
 }
