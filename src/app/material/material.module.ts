710157a03dd5c6be0d7cac20e05c5a085bc388cc
Krit <kritdevelop@gmail.com>
2021-07-01 16:14:28 +0700

    add menu item in sidebar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
 
 const MaterialComponents = [
   MatButtonModule,
   MatCardModule,
-  MatIconModule
+  MatIconModule,
+  MatTreeModule
 ]
 
 @NgModule({
