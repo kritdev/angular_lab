8bd5c63fd10dfdcb83a5b5c26cd5db9a75f3efe1
Krit <kritdevelop@gmail.com>
2021-07-03 06:26:25 +0700

    create page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { MainComponent } from './layout/main/main.component';
+import { AboutComponent } from './page/about/about.component';
+import { SkillComponent } from './page/skill/skill.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { PortfolioComponent } from './page/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainComponent
+    MainComponent,
+    AboutComponent,
+    SkillComponent,
+    ContactComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
