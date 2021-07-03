f582a38a16c5e5dc3ee8e4eecc71685b44946b7c
Krit <kritdevelop@gmail.com>
2021-07-04 06:04:52 +0700

    add product detail

--- src/app/page/portfolio/portfolio.component.ts
+++ src/app/page/portfolio/portfolio.component.ts
@@ -10,34 +10,34 @@ export class PortfolioComponent implements OnInit {
 
   productList: IProduct[] = [
     {
-      name: 'P1',
+      name: 'เก้าอี้ไม้เล็ก (กลม)',
       price: 550,
       imageUrl: 'assets/img/p1.png',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies justo nibh. Vestibulum convallis tempor ex, non tempus nibh vestibulum porttitor. Nullam est elit, mattis id condimentum' 
     },
     {
-      name: 'P2',
+      name: 'เก้าอี้ไม้เล็ก (เหลี่ยม)',
       price: 550,
       imageUrl: 'assets/img/p2.png',
-      description: 'Descript 1' 
+      description: 'Sed ultricies justo nibh. Vestibulum convallis tempor ex, non tempus nibh vestibulum porttitor. Nullam est elit, mattis id condimentum' 
     },
     {
-      name: 'P3',
-      price: 550,
+      name: 'เก้าอี้ไม้ (เหลี่ยม)',
+      price: 890,
       imageUrl: 'assets/img/p3.png',
-      description: 'Descript 1' 
+      description: 'Vestibulum convallis tempor ex, non tempus nibh vestibulum porttitor. Nullam est elit, mattis id condimentum' 
     },
     {
-      name: 'P4',
-      price: 550,
+      name: 'เก้าอี้ไม้ (กลม) ขากลม',
+      price: 990,
       imageUrl: 'assets/img/p4.png',
-      description: 'Descript 1' 
+      description: 'Non tempus nibh vestibulum porttitor. Nullam est elit, mattis id condimentum' 
     },
     {
-      name: 'P5',
-      price: 550,
+      name: 'เก้าอี้ไม้ (กลม) ขาเหลี่ยม',
+      price: 990,
       imageUrl: 'assets/img/p5.png',
-      description: 'Descript 1' 
+      description: 'Nullam est elit, mattis id condimentum. Sed ultricies justo nibh. ' 
     },
   ]
 
