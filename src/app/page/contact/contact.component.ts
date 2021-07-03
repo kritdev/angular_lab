d488a259490e745d1be8b513b5ed1eb005126ecd
Krit <kritdevelop@gmail.com>
2021-07-04 06:27:14 +0700

    create data object for contact page (sorting issue)

--- src/app/page/contact/contact.component.ts
+++ src/app/page/contact/contact.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  data = {
+    'Phone:':'66-099-999-9999',
+    'Email:':'thai.codecamp@gmail.com',
+    'Line ID:':'codecamp',
+    'Website:':'www.codecampthailand.com',
+    'Address:':'99/31 Moo4 Software Park Building, Chaeng Wattana Road, Klong Gleua, Pakkred, Nonthaburi 11120'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
