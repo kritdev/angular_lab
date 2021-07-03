4b865d9dcd07b767af553629b282ce92965aafaa
Krit <kritdevelop@gmail.com>
2021-07-04 06:13:04 +0700

    create data object for about page

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  data = {
+    imageUrl: 'assets/img/JoneSalad.png',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi temporibus quidem asperiores earum assumenda recusandae nesciunt vitae quos! Sed reprehenderit error voluptas consequuntur aliquid ipsum aliquam sunt voluptates odio animi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi temporibus quidem asperiores earum assumenda recusandae nesciunt vitae quos! Sed reprehenderit error voluptas consequuntur aliquid ipsum aliquam sunt voluptates odio animi.'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
