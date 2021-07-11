5cfdb39b6b20ae5596053e01fbde45f85699ce19
Krit <kritdevelop@gmail.com>
2021-07-11 08:15:06 +0700

    paging component - display item in page

--- src/app/component/paging/paging.component.ts
+++ src/app/component/paging/paging.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-paging',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagingComponent implements OnInit {
 
+  @Input() totalItem = 1;
+  @Input() itemPerPage = 1;
+
+  activePage = 2;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getFirstItemInPage() {
+    return this.itemPerPage * (this.activePage - 1) + 1;
+  }
+
+  getLastItemInPage() {
+    const result = this.itemPerPage * (this.activePage);
+    return result < this.totalItem ? result : this.totalItem;
   }
 
 }
