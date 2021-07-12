8d04fc38c3518129be5049f71c0c159dd48b0f7a
Krit <kritdevelop@gmail.com>
2021-07-12 10:34:18 +0700

    footer component - create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NewsCardComponent } from './component/news-card/news-card.component';
 import { StatisticCardComponent } from './component/statistic-card/statistic-card.component';
 import { TableCardComponent } from './component/table-card/table-card.component';
 import { PagingComponent } from './component/paging/paging.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { PagingComponent } from './component/paging/paging.component';
     NewsCardComponent,
     StatisticCardComponent,
     TableCardComponent,
-    PagingComponent
+    PagingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
